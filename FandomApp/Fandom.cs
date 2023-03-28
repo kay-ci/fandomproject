1962395023b4464dee7a96f55db7b14436bfe880
Fred-D <fred-dgennie.louis@dawsoncollege.qc.ca>
2023-03-28 15:57:27 -0400

    fandom property validation

--- FandomApp/Fandom.cs
+++ FandomApp/Fandom.cs
@@ -1,15 +1,17 @@
 public class Fandom
 {
-    public string? Name {get; set;}
-    public string? Category {get; set;}
+    public string Name {get; set;}
+    public string Category {get; set;}
     public string? Description {get; set;}
 
     //constructor
-    public Fandom() {
-
+    public Fandom(string name, string category, string description) {
+        this.Name = name;
+        this.Category = category;
+        this.Description = description;
     }
 
-    public override string? ToString() {
-        throw new NotImplementedException();
+    public override string ToString() {
+        return $"Fandom name: {this.Name}, category: {this.Category} \nDescription: {this.Description}";
     }
 }
