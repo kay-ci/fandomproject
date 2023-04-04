b2676804be62af09d8fb5b6051a7d3721293b671
Kayci Nicole Davila <kayci.davila@dawsoncollege.qc.ca>
2023-04-04 14:11:40 -0400

    Added method ChangeUsername in User)

--- FandomApp/User.cs
+++ FandomApp/User.cs
@@ -2,32 +2,51 @@ using System.Text.RegularExpressions;
 
 namespace userInfo{
     public class User{
-        public string? UserName {get; set;}
+        public string? Username {get; set;}
         public Profile? UserProfile {get; set;}
         public UserMessage Messages {get; set;}
 
         public List<Event> Events {get;}
         
-        //constructor
+        //constructors
         public User(string userName, Profile userProfile, List<Event> events){
-            UserName = userName;
+            Username = userName;
             UserProfile = userProfile;
             Events = events;
             Messages = new UserMessage(this);
         }
         public User(string userName, Profile userProfile){
-            UserName = userName;
+            Username = userName;
             UserProfile = userProfile;
             Messages = new UserMessage(this);
         }
 
         //This method will allow a user to change their password
-        public void ChangePassword(Login user, string newPassword){
+        public void ChangePassword(Login userManager, string newPassword){
+            if (string.IsNullOrWhiteSpace(newPassword)){
+                throw new ArgumentException("new password cannot be null");
+            };
             
+            // set the currenly logged in users password
         }
 
         //This method allows a user to change username
-        public void ChangeUsername(){
+        public void ChangeUsername(Login userManager, string newUsername){
+            if (string.IsNullOrWhiteSpace(Username)){
+                throw new ArgumentException("new username cannot be null");
+            }
+
+            //check newUsername is 1 word (numbers allowed)
+            Regex pattern = new Regex("[A-Za-Z0-9]");
+            if (!pattern.IsMatch(newUsername)){
+                throw new ArgumentException("Username can only be 1 word!");
+            }
+            
+            //check that it doesnt match another username in database
+
+            //set username
+            userManager.CurrentUser.Username = newUsername;
+
 
         }
 
