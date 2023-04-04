02504105326348034eefc45e5104a4b70fb48495
Karekin <karekin.kiyici@dawsoncollege.qc.ca>
2023-04-04 14:12:59 -0400

    Halfway done!

---
+++ FandomApp/Login.cs
@@ -0,0 +1,13 @@
+using userInfo;
+public class Login {
+
+    public User CurrentUser;
+
+    public Login(User CurrentUser){
+        this.CurrentUser = CurrentUser;
+    }
+
+    public void Register_User(){
+        //Now we register the user that is logged in (CurrentUser)
+    }
+}
