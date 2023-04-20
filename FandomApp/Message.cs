22fadbf91bfcbfad78d447ca3f4bc79946d1730c
Karekin <karekin.kiyici@dawsoncollege.qc.ca>
2023-04-20 17:38:12 -0400

    Changed Message and UserMessage to work better with EF. Message also has a title field now and I filled in the sort method inside of UserMessage that works with the title field of Message

--- FandomApp/Message.cs
+++ FandomApp/Message.cs
@@ -1,38 +1,24 @@
 namespace UserInfo{
     public class Message{
-<<<<<<< HEAD
-        public int id {get; set;}
-        public UserMessage sender {get; set;}
-        public List<UserMessage> recipients = new List<UserMessage>();
-        public DateTime timesent {get; set;}
-        public string text {get; set;}
-=======
         public int Id {get; set;}
-        public User Sender {get; set;} = null!;
-        public List<User> Recipients = new();
+        public UserMessage Sender {get; set;} = null!;
+        public List<UserMessage> Recipients = new();
         public DateTime Timesent {get; set;}
         public string Text {get; set;}
->>>>>>> de2e9a883ab19bb15c3d05d34bec3f377f2f861b
+        public string Title {get; set;}
         //This field determines if the message has been read or not. Will get updated by UserMessage
         public bool Seen {get; set;}
 
         private Message(){}
         //Basic constructor, validation done in UserMessage for text
-<<<<<<< HEAD
-        public Message(UserMessage sender, List<UserMessage> recipients, string text){
-            this.sender = sender;
-            this.timesent = DateTime.Now;
-            this.text = text;
-            this.seen = false;
-=======
-        public Message(User sender, List<User> recipients, string text){
+        public Message(UserMessage sender, List<UserMessage> recipients, string text, string title){
             this.Sender = sender;
             this.Timesent = DateTime.Now;
             this.Text = text;
+            this.Title = title;
             this.Seen = false;
-            this.Recipients = new List<User>();
->>>>>>> de2e9a883ab19bb15c3d05d34bec3f377f2f861b
-            foreach(User user in recipients){
+            this.Recipients = new List<UserMessage>();
+            foreach(UserMessage user in recipients){
                 this.Recipients.Add(user);
             }
         }
--- FandomApp/UserMessage.cs
+++ FandomApp/UserMessage.cs
@@ -15,7 +15,7 @@ namespace UserInfo{
         [InverseProperty("Sender")]
         public List<Message> Outbox {get; set;}
 
-        //Here we have three constructors. This one is for the DBContext
+        //Here we have two constructors. This one is for the DBContext
         private UserMessage(){}
         //This one is for a completely new user
         public UserMessage(User user){
@@ -23,26 +23,16 @@ namespace UserInfo{
             Inbox = new List<Message>();
             Outbox = new List<Message>();
         }
-        //This method would be used when we are taking a user from the database.
-        //Because they already exist, they would have an Inbox and an Outbox already
-        public UserMessage(User user, List<Message> inbox, List<Message> outbox){
-            this.user = user;
-            Inbox = new List<Message>();
-            Outbox = new List<Message>();
-            foreach(Message msg in inbox){ this.Inbox.Add(msg);}
-            foreach(Message msg in outbox){ this.Outbox.Add(msg);}
-        }
-
-        public void CreateMessage(List<User> recipients, string text){
+        public void CreateMessage(List<UserMessage> recipients, string text, string title){
             //We check if the text is null or empty
             if(string.IsNullOrWhiteSpace(text)) throw new ArgumentException("Text must be filled with text!");
             //Create the message and add it to our Outbox
-            Message newMsg = new Message(this.user, recipients, text);
+            Message newMsg = new Message(this.user.Messages, recipients, text, title);
             Outbox.Add(newMsg);
             //Here we would add the message to the recipients Inboxes... probably will use the AddMessage method below
             //So, something like:
-            foreach(User recipient in recipients){
-                recipient.Messages.AddMessage(newMsg);
+            foreach(UserMessage recipient in recipients){
+                recipient.AddMessage(newMsg);
             }
 
         }
@@ -51,6 +41,9 @@ namespace UserInfo{
         //Afterwards, we set the bool for if the message has been read to true using MessageIsRead()
         //Finally, we return the text inside of the message. Needs validation for the index
         public string ReadMessage(int index){
+            if(index >= this.Inbox.Count){
+                throw new ArgumentException("ERROR, index given is larger than amount of messages this user has in their Inbox");
+            }
             Inbox[index].MessageIsRead();
             return Inbox[index].Text;
         }
@@ -61,8 +54,34 @@ namespace UserInfo{
             this.Inbox.Add(newMsg);
         }
 
-        //This method sorts all of the messages by alphabet (of the sender's name)
+        //This method sorts all of the messages by their title in alphabetical order
         //or by date sent (oldest or newest first)
-        public void SortMessages(){}
+        public void SortMessages(bool sortInbox, bool sortByDate, bool newestFirst){
+
+            if(sortInbox){
+                if(sortByDate && newestFirst){
+                    //This will sort out the list of messages by newest to oldest
+                    Inbox.Sort((x, y) => DateTime.Compare(x.Timesent, y.Timesent));
+                }else if(sortByDate && newestFirst == false){
+                    //This will sort out the list of messages by oldest to newest
+                    Inbox.Sort((x, y) => DateTime.Compare(y.Timesent, x.Timesent));
+                }else {
+                    //This will sort out the list of messages by title in alphabetical order
+                    Inbox.Sort((x, y) => String.Compare(x.Title, y.Title));
+                }
+            }else {
+                if(sortByDate && newestFirst){
+                    //This will sort out the list of messages by newest to oldest
+                    Outbox.Sort((x, y) => DateTime.Compare(x.Timesent, y.Timesent));
+                }else if(sortByDate && newestFirst == false){
+                    //This will sort out the list of messages by oldest to newest
+                    Outbox.Sort((x, y) => DateTime.Compare(y.Timesent, x.Timesent));
+                }else {
+                    //This will sort out the list of messages by title in alphabetical order
+                    Outbox.Sort((x, y) => String.Compare(x.Title, y.Title));
+                }
+            }
+
+        }
     }
 }
