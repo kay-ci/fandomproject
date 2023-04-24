4132dc8adeebbe5a836bfa12aca8a331dca7802a
Kayci Nicole Davila <kayci.davila@dawsoncollege.qc.ca>
2023-04-24 19:12:48 -0400

    setup UserService to only be insantiated once

--- FandomApp/UserService.cs
+++ FandomApp/UserService.cs
@@ -1,5 +1,25 @@
+namespace UserInfo;
 using UserInfo;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+public class UserService{
+    private static UserService? _instance;
+    private FanAppContext _context = null!;
+    private UserService(){
+    }
+    
+    public static UserService getInstance(){
+        if(_instance is null){
+            _instance = new UserService();
+        }
+        return _instance;
+    }
+
+    public void setLibraryContext(FanAppContext context){
+        _context = context;
+    }
+
+}
