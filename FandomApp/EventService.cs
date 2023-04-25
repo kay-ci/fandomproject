b30cf89c2541cf97502f832f5f7834a53f9d9e90
Fred-D <fred-dgennie.louis@dawsoncollege.qc.ca>
2023-04-25 16:29:05 -0400

    ef

--- FandomApp/EventService.cs
+++ FandomApp/EventService.cs
@@ -1,9 +1,18 @@
-
+using UserInfo;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 public class EventService
 {
+    private FanAppContext _context = null!;
     public EventService() 
     {
+    }
 
+    public void setLibraryContext(FanAppContext context) 
+    {
+        _context = context;
     }
     
 }
