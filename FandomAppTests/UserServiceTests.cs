b6305fd7ff661b65cc707109295c09bec8ae86d3
Kayci Nicole Davila <kayci.davila@dawsoncollege.qc.ca>
2023-04-24 23:10:04 -0400

    Added createAccont and GetUser, getuser is tested and passed

--- FandomAppTests/UserServiceTests.cs
+++ FandomAppTests/UserServiceTests.cs
@@ -38,4 +38,34 @@ public class UserServiceTests{
         Assert.AreEqual(16, users[2].UserProfile.Age);
     }
 
+     [TestMethod]
+    public void GetUserTest()
+    {
+        //Arrange
+        var listdata = new List<User>();
+        listdata.Add(new User("KayciUsername", new Profile("Kayci", "she/her", 19, "Canada", "Montreal")));
+        listdata.Add(new User("liliUsername", new Profile("Lili", "they/them", 20, "Ireland", "Dublin")));
+        listdata.Add(new User("bestUser", new Profile("Jim", "he/him", 16, "Canada", "Laval")));
+
+        var data = listdata.AsQueryable();
+
+        var mockSet = new Mock<DbSet<User>>();
+        mockSet.As<IQueryable<User>>().Setup(m => m.Provider).Returns(data.Provider);
+        mockSet.As<IQueryable<User>>().Setup(m => m.Expression).Returns(data.Expression);
+        mockSet.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(data.ElementType);
+        mockSet.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+ 
+        var mockContext = new Mock<FanAppContext>();
+        mockContext.Setup(m => m.FandomUsers).Returns(mockSet.Object);
+        
+        var service = UserService.getInstance();
+        service.setLibraryContext(mockContext.Object);
+
+        //Act
+        User user = service.GetUser("KayciUsername");
+
+        //Assert
+        Assert.AreEqual("KayciUsername", user.Username);
+        Assert.AreEqual(19, user.UserProfile.Age);
+    }
 }
