01b34f73705dd5b2877c65945b0384c5ebff010a
Fred-D <fred-dgennie.louis@dawsoncollege.qc.ca>
2023-04-25 23:39:18 -0400

    GetAllEvents() and DeleteEvents() added

--- FandomApp/EventService.cs
+++ FandomApp/EventService.cs
@@ -6,52 +6,71 @@ using System.Threading.Tasks;
 public class EventService
 {
     private FanAppContext _context = null!;
-    public EventService() 
-    {
-    }
-
-    public void setLibraryContext(FanAppContext context) 
+    public EventService(FanAppContext context) 
     {
         _context = context;
     }
 
     //this method add the event to the database
-    public void CreateEvent(Event new_event) {
-
-        try
+    public void CreateEvent(Event new_event) 
+    {
+        if (GetEvent(new_event.EventId) is null)
         {
             _context.Events.Add(new_event);
             _context.SaveChanges();
+            Console.WriteLine("New event added to database!");
         }
-        catch (System.Exception)
-        {
-           Console.Write("An error happened!");
-        }
-        
+       
     }
 
     //this method retrieve a single event using it's ID
-    public Event GetEvent(int eventID)
+    public Event? GetEvent(int eventID)
     {
-        var query = from events in _context.Events 
-            where events.EventId == eventID
-            select events;
-        
-        var eventFound = query.First<Event>();
+        Event? eventFound = null;
+        try
+        {
+            var eventQuery = 
+                from events in _context.Events 
+                where events.EventId == eventID
+                select events;
+
+            eventFound = eventQuery.First<Event>();
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine("No event was found");
+        }
+
         return eventFound;
+    }
+
+    //this method gets all the events
+    public List<Event> GetAllEvents()
+    {
+        List<Event> events = _context.Events
+            .Include(e => e.Categories)
+            .ToList<Event>();
+        
+        return events;
     }
 
     //this method allow owner of event to edit it
     public void EditEvent(User user, Event fandomEvent) {
-        if (true)
-        {
-            
-        }
+        
+        var oldEvent = GetEvent(fandomEvent.EventId);
+        
+
+    }
+
+    //this method remove eventfrom database
+    public void DeleteEvent(User user, Event fandomEvent) {
+        _context.Events.Remove(fandomEvent);
+        _context.SaveChanges();
     }
 
     //this method will print out the list of attendees
     //may not be needed because it's not a console app
-    public void ShowAttendees() {
+    public List<User> GetEventAttendees() {
         throw new NotImplementedException();
     }
 
--- FandomAppTests/EventServiceTests.cs
+++ FandomAppTests/EventServiceTests.cs
@@ -1,35 +1,30 @@
 using UserInfo;
+using Moq;
+using Microsoft.EntityFrameworkCore;
 namespace FandomAppTests;
 
 [TestClass]
 public class EventServiceTests{
     [TestMethod]
-    //[ExpectedException(typeof(ArgumentException))] 
-    public void GetEvent_Event_ThrowException()
+    public void CreateEvent_NewEvent()
     {
         //Arrange
-        List<Category> categories = new List<Category>();
-        List<Fandom> fandoms = new List<Fandom>();
-        List<Badge> badges = new List<Badge>();
-
-        //List<Event> events = new List<Event>();
-    
-        //Profile attendee_profile = new Profile("User", "they/them", 15, "Canada", "Montreal", categories, fandoms, badges, "description", "pictures", "interests");
-        Profile owner_profile = new Profile("Owner", "they/them", 21, "Canada", "Montreal", categories, fandoms, badges, "description", "pictures", "interests");
-
-        //User attendee = new User("usertest", attendee_profile, events);
+        Profile owner_profile = new Profile("Owner", "they/them", 21, "Canada", "Montreal", new List<Category>(),  new List<Fandom>(), new List<Badge>(), "description", "pictures", "interests");
         User owner = new User("Owner", owner_profile);
 
-        DateTime date = new DateTime(2023, 12, 12);
-        Event event_test = new Event("title", date, "location", categories, 18, owner);
-        FanAppContext db = new FanAppContext();
+        Event event_test = new Event("title", new DateTime(2023, 12, 12), "location", new List<Category>(), 18, owner);
 
-        EventService ES = new EventService();
+        var mockSet = new Mock<DbSet<Event>>();
+        var mockContext = new Mock<FanAppContext>();
+        mockContext.Setup(m => m.Events).Returns(mockSet.Object);
+
+        EventService ES = new EventService(mockContext.Object);
+        
+        //Act
         ES.CreateEvent(event_test);
 
-        //Act
-
         //Assert
-
+        mockSet.Verify(m => m.Add(It.IsAny<Event>()), Times.Once());
+        mockContext.Verify(m => m.SaveChanges(), Times.Once());
     }
 }
