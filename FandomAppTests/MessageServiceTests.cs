3d257877bbb3ee7eff733e24565f52551a902127
Karekin <karekin.kiyici@dawsoncollege.qc.ca>
2023-04-26 00:57:21 -0400

    Stuck on problem for tests, will revisit later

--- FandomAppTests/MessageServiceTests.cs
+++ FandomAppTests/MessageServiceTests.cs
@@ -14,9 +14,13 @@ public class MessageServiceTests{
         listdata.Add(new UserMessage(new User("bestUser", new Profile("Jim", "he/him", 16, "Canada", "Laval"))));
 
         User sender = new User("kare", new Profile("Karekin", "he/him", 22, "Canada", "Montreal"));
+        var recipients2 = new List<UserMessage>();
+        recipients2.Add(listdata[1]);
+        var recipients3 = new List<UserMessage>();
+        recipients3.Add(listdata[2]);
         sender.Messages.CreateMessage(listdata, "test", "testTitle");
-        sender.Messages.CreateMessage(new List<UserMessage>(listdata[1]), "test2", "test2Title");
-        sender.Messages.CreateMessage(listdata, "test3", "test3Title");
+        sender.Messages.CreateMessage(recipients2, "test2", "test2Title");
+        sender.Messages.CreateMessage(recipients3, "test3", "test3Title");
 
         var data = listdata.AsQueryable();
 
@@ -33,7 +37,52 @@ public class MessageServiceTests{
         service.setLibraryContext(mockContext.Object);
 
         //Act
-        
+        var Inbox_Test = new List<Message>();
+        Inbox_Test = service.GetBox(listdata[0].UserMessageId, true);
+
+        //Assert
+        Assert.AreEqual("test", Inbox_Test[0].Text);
+    }
+
+    [TestMethod]
+    public void Get_Outbox_Test_Succeed(){
+        //Arrange
+        var listdata = new List<UserMessage>();
+        listdata.Add(new UserMessage(new User("KayciUsername", new Profile("Kayci", "she/her", 19, "Canada", "Montreal"))));
+        listdata.Add(new UserMessage(new User("liliUsername", new Profile("Lili", "they/them", 20, "Ireland", "Dublin"))));
+        listdata.Add(new UserMessage(new User("bestUser", new Profile("Jim", "he/him", 16, "Canada", "Laval"))));
+
+        User sender = new User("kare", new Profile("Karekin", "he/him", 22, "Canada", "Montreal"));
+        var recipients2 = new List<UserMessage>();
+        recipients2.Add(listdata[1]);
+        var recipients3 = new List<UserMessage>();
+        recipients3.Add(listdata[2]);
+        sender.Messages.CreateMessage(listdata, "test", "testTitle");
+        sender.Messages.CreateMessage(recipients2, "test2", "test2Title");
+        sender.Messages.CreateMessage(recipients3, "test3", "test3Title");
+
+        listdata.Add(sender.Messages);
+
+        var data = listdata.AsQueryable();
+
+        var mockSet = new Mock<DbSet<UserMessage>>();
+        mockSet.As<IQueryable<UserMessage>>().Setup(m => m.Provider).Returns(data.Provider);
+        mockSet.As<IQueryable<UserMessage>>().Setup(m => m.Expression).Returns(data.Expression);
+        mockSet.As<IQueryable<UserMessage>>().Setup(m => m.ElementType).Returns(data.ElementType);
+        mockSet.As<IQueryable<UserMessage>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+        var mockContext = new Mock<FanAppContext>();
+        mockContext.Setup(m => m.FandomUserMessages).Returns(mockSet.Object);
+
+        var service = MessageService.getInstance();
+        service.setLibraryContext(mockContext.Object);
+
+        //Act
+        var Outbox_Test = new List<Message>();
+        Outbox_Test = service.GetBox(sender.Messages.UserMessageId, false);
+
+        //Assert
+        Assert.AreEqual("test3", Outbox_Test[2].Text);
     }
 
     
