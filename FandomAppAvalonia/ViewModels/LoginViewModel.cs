5f2a0eb94790903bc710b109b2df25dc06193192
Kayci Nicole Davila <kayci.davila@dawsoncollege.qc.ca>
2023-05-08 11:10:57 -0400

    Added some initial setup for reference and added instructions for teammates on how to create views

---
+++ FandomAppAvalonia/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+using FandomApp;
+using ReactiveUI;
+using System.Reactive;
+namespace FandomAppSpace.ViewModels;
+public class LoginViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
