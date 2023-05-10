c7b50cda73960201e36fe3784376da0c68073731
Fred-D <fred-dgennie.louis@dawsoncollege.qc.ca>
2023-05-10 17:43:28 -0400

    EditMessage of service done

--- FandomApp/MessageService.cs
+++ FandomApp/MessageService.cs
@@ -56,64 +56,14 @@ public class MessageService {
             throw new ArgumentException("Only the creator of this message can modify it.");
         }
         
-        if (GetMessage(updatedmessage.Title) != null)
+        //Verifying that the message wasnt sent
+        if (updatedmessage.Sent != true)
         {
             _context.FandomMessages.Update(updatedmessage);
             _context.SaveChanges();
+        } else {
+            throw new ArgumentException("Can't edit a message that was already sent!");
         }
     }
-    
-    public void Delete_Message(Message message) {
-        _context.FandomMessages.Remove(message);
-        _context.SaveChanges();
-    }
 
-    //public UserMessage GetUserMessage(User usr){
-    //    var query = from u_msg in _context.FandomUserMessages
-    //        where u_msg.Owner.Equals(usr)
-    //        select u_msg;
-    //    var fetchedUMSG = query.First<UserMessage>();
-    //    return fetchedUMSG;
-    //}
-
-    //public List<Message> GetBox(User usr, bool getInbox){
-    //    var query = from u_msg in _context.FandomUserMessages
-    //        where u_msg.Owner.Equals(usr)
-    //        select u_msg;
-    //    var fetchedUMSG = query.First<UserMessage>();
-    //    if(getInbox) return fetchedUMSG.Inbox;
-    //    return fetchedUMSG.Outbox;
-    //}
-
-    //public void Add_UserMessage(User user){
-    //    var user_message = user.Messages;
-//
-    //    _context.FandomUserMessages.Add(user_message);
-    //    _context.SaveChanges();
-    //}
-
-    
-
-    //public void Update_UserMessage(User user){
-    //    var user_message = new UserMessage(user);
-    //    var query = from u_msg in _context.FandomUserMessages
-    //        where u_msg.Owner.Equals(user)
-    //        select u_msg;
-    //    var fetchedUMSG = query.First<UserMessage>();
-    //    
-    //    fetchedUMSG.Inbox = new List<Message>();
-    //    fetchedUMSG.Outbox = new List<Message>();
-    //    foreach(Message msg in user_message.Inbox) fetchedUMSG.Inbox.Add(msg);
-    //    foreach(Message msg in user_message.Outbox) fetchedUMSG.Outbox.Add(msg);
-    //    _context.SaveChanges();
-    //}
-
-    
-
-    //public void Delete_UserMessage(UserMessage u_msg){
-    //    _context.FandomUserMessages.Remove(u_msg);
-    //    _context.SaveChanges();
-    //}
-
-    
 }
