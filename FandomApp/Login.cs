9e9343da1b3722da9643e387f5eaa16ce905a22c
Fred-D <fred-dgennie.louis@dawsoncollege.qc.ca>
2023-05-10 21:16:19 -0400

    merge

---
+++ FandomApp/Login.cs
@@ -0,0 +1,12 @@
+using UserInfo;
+using Microsoft.EntityFrameworkCore;
+
+public class Login {
+
+    public User? CurrentUser;
+
+    public Login(User CurrentUser){
+        this.CurrentUser = CurrentUser;
+    }
+
+}
