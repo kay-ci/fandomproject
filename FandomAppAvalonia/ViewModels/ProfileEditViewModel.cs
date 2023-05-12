97bf7f0e4a94ae3a4ca054f3b15c37ee2fb2a8dc
Kayci Nicole Davila <kayci.davila@dawsoncollege.qc.ca>
2023-05-12 00:14:43 -0400

    edit in progress

--- FandomAppAvalonia/ViewModels/ProfileEditViewModel.cs
+++ FandomAppAvalonia/ViewModels/ProfileEditViewModel.cs
@@ -4,6 +4,8 @@ using FandomAppSpace;
 using ReactiveUI;
 using UserInfo;
 using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+
 namespace FandomAppSpace.ViewModels
 {
     public class ProfileEditViewModel : ViewModelBase
@@ -13,6 +15,14 @@ namespace FandomAppSpace.ViewModels
         string _fandomCategory;
         string _fandomDescription;
         string _badgesText;
+        string _name;
+        string _pronouns;
+        int _age;
+        string _country;
+        string _city;
+        string _description;
+        string _interests;
+        string _picture;
         public string CategoryText 
         {
             get => _categoryText;
@@ -38,6 +48,46 @@ namespace FandomAppSpace.ViewModels
             get => _badgesText;
             private set => this.RaiseAndSetIfChanged(ref _badgesText, value);
         }
+        public string Name 
+        {
+            get => _name;
+            private set => this.RaiseAndSetIfChanged(ref _name, value);
+        }
+         public string Pronouns 
+        {
+            get => _pronouns;
+            private set => this.RaiseAndSetIfChanged(ref _categoryText, value);
+        }
+         public int Age 
+        {
+            get => _age;
+            private set => this.RaiseAndSetIfChanged(ref _age, value);
+        }
+         public string Country 
+        {
+            get => _country;
+            private set => this.RaiseAndSetIfChanged(ref _country, value);
+        }
+         public string City 
+        {
+            get => _city;
+            private set => this.RaiseAndSetIfChanged(ref _city, value);
+        }
+         public string Description 
+        {
+            get => _description;
+            private set => this.RaiseAndSetIfChanged(ref _description, value);
+        }
+         public string Interests 
+        {
+            get => _interests;
+            private set => this.RaiseAndSetIfChanged(ref _interests, value);
+        }
+         public string Picture 
+        {
+            get => _picture;
+            private set => this.RaiseAndSetIfChanged(ref _picture, value);
+        }
         ObservableCollection<Category> Categories {get; set;}
         ObservableCollection<Fandom> Fandoms {get; set;}
         ObservableCollection<Badge> Badges {get; set;}
@@ -53,16 +103,20 @@ namespace FandomAppSpace.ViewModels
             Categories = new ObservableCollection<Category>(CategoriesList);
             Fandoms = new ObservableCollection<Fandom>(FandomsList);
             Badges = new ObservableCollection<Badge>(BadgesList);
-            var editEnabled = this.WhenAnyValue(
-                x => x.FandomCategory, x=> x.FandomName, x=>x.FandomCategory, x=>x.FandomDescription, x=>x.BadgesText, x=>x.Profile.Name, x=>x.Profile.Pronouns, x=>x.Profile.Country, x=>x.Profile.City,x=>x.Profile.Description, x=>x.Profile.Age,x=>x.Profile.Picture, x=>x.Profile,
-                (user_name, pass_word, country, city, name, age, pronouns) =>
-                    !string.IsNullOrWhiteSpace(user_name) &&
-                    !string.IsNullOrWhiteSpace(pass_word) &&
-                    !string.IsNullOrWhiteSpace(country) &&
-                    !string.IsNullOrWhiteSpace(city) &&
-                    !string.IsNullOrWhiteSpace(name) &&
-                    !string.IsNullOrWhiteSpace(pronouns))
-                .DistinctUntilChanged();
+            // var editEnabled = this.WhenAnyValue(
+            //     x => x.FandomCategory, x=> x.FandomName, x=>x.FandomDescription, x=>x.BadgesText, x=>x.Name, x=>x.Pronouns, x=>x.Country, x=>x.City,x=>x.Description, x=>x.Age, x=>x.Picture, x=>x.Interests,
+            //     (fcategory, fname, fdescription, badges, pname, pronouns, country, city, pdescription, age, picture, interests) =>
+            //         !string.IsNullOrWhiteSpace(fcategory) &&
+            //         !string.IsNullOrWhiteSpace(fname) &&
+            //         !string.IsNullOrWhiteSpace(fdescription) &&
+            //         !string.IsNullOrWhiteSpace(badges) &&
+            //         !string.IsNullOrWhiteSpace(pname) &&
+            //         !string.IsNulOrWhiteSpace(pronouns)&&
+            //         !string.IsNullOrWhiteSpace(country)&&
+            //         !string.IsNullOrWhiteSpace(city)&&
+            //         !string.IsNullOrWhiteSpace(picture)&&
+            //         age >= 0 && age <= 130)
+            // .DistinctUntilChanged();
             Ok = ReactiveCommand.Create(() => { });
         }
 
